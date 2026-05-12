ce9c0a24550480c152e83d4c03e8e86871c049d2
Nahid1828 <isam15-5334@diu.edu.bd>
2026-05-12 23:22:28 +0600

    Implement server & connected to MongoDB

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const PORT = process.env.PORT;
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = process.env.DB_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(PORT, () => {
+  console.log(`Server on running port ${PORT}`);
+});
